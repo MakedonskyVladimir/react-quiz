04f24490e65997d65fb35bdf2601478918320592
MakedonskyVladimir <shurmanoff@mail.ru>
2022-12-16 11:52:58 +0300

    added three questions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
 
+const questions = [
+  {
+    title: 'Реакт - это ...?', 
+    variants: ['библиотека', 'фреймворк', 'приложение'], 
+    correct: 0,
+  }, 
+  {
+    title: 'Компонент - это ...?', 
+    variants: ['приложение', 'часть приложения', 'сайт'], 
+    correct: 1,
+  }, 
+  {
+    title: 'JSX - это', 
+    variants: ['библиотека', 'фреймворк', 'приложение'], 
+    correct: 2,
+  }, 
+]
+
 const App = () => {
   return (
     <>
-      <h1>Тест</h1>
+      <h1>{questions[0].title}</h1>
     </>
   )
 
