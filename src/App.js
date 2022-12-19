43eca6edf8ca6bc1488d174b9b78cc7eb72d9927
MakedonskyVladimir <shurmanoff@mail.ru>
2022-12-19 16:06:47 +0300

    added progress-bar

--- src/App.js
+++ src/App.js
@@ -19,20 +19,23 @@ const questions = [
   }, 
 ]
 
-const Game = ({question}) => {
+const Game = ({question, onCliclVariant, step}) => {
+  const percentage = Math.round ((step / questions.length) * 100)
+  console.log(percentage)
+
   return (
     <div>
       <div className='progress'>
-        <div style={{width: '50%'}} className="progress_inner">-------прогресс-бар-------</div>
+        <div style={{width: `${percentage}%`}} className="progressinner"></div>
       </div>
-      <h3>{question.title}</h3>
+      <h1>{question.title}</h1>
       <ul>
         {
-          question.variants.map((text) => (
-            <li key={text}>{text}</li>
+          question.variants.map((variant, index) => (
+            <li onClick={() => onCliclVariant(index)} key={index}>{variant}</li>
           ))}
       </ul>
-  </div>
+    </div>
   )
 
 }
@@ -40,20 +43,25 @@ const Game = ({question}) => {
 const Result = () => {
   return (
     <div className='result'>
-      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" width="200px"/>      <h1>Результат 5 из 10...</h1>
+      {/* <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png"/>       */}
+      <h2>Результат 5 из 10...</h2>
       <button>Попробовать еще...</button>
   </div>
   )
-
 }
 
 const App = () => {
   const [step, setStep] = React.useState(0)
   const question = questions[step]
 
+  const onCliclVariant = (index) => {
+    console.log(step, index);
+    setStep(step + 1);
+  }
+
   return (
     <div className='App'>
-      <Game question={question}/>
+      <Game step={step} question={question} onCliclVariant={onCliclVariant}/>
       <Result />
     </div>
   )
